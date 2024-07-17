4c8f36318cf5e41440a74e19f7f26f7ff150603d
ursenpai6581 <ursenpai6581@gmail.com>
2024-07-17 18:02:30 +0000

    first commit

---
+++ web/main.js
@@ -0,0 +1,44 @@
+import { streamGemini } from './gemini-api.js';
+
+let form = document.querySelector('form');
+let promptInput = document.querySelector('input[name="prompt"]');
+let output = document.querySelector('.output');
+
+form.onsubmit = async (ev) => {
+  ev.preventDefault();
+  output.textContent = 'Thinking....';
+
+  try {
+ 
+    let imageUrl = form.elements.namedItem('chosen-image').value;
+    let imageBase64 = await fetch(imageUrl)
+      .then(r => r.arrayBuffer())
+      .then(a => base64js.fromByteArray(new Uint8Array(a)));
+
+    let contents = [
+      {
+        role: 'user',
+        parts: [
+          { inline_data: { mime_type: 'image/jpeg', data: imageBase64, } },
+          { text: promptInput.value }
+        ]
+      }
+    ];
+
+
+    let stream = streamGemini({
+      model: 'gemini-1.5-pro',
+      contents,
+    });
+
+
+    let buffer = [];
+    let md = new markdownit();
+    for await (let chunk of stream) {
+      buffer.push(chunk);
+      output.innerHTML = md.render(buffer.join(''));
+    }
+  } catch (e) {
+    output.innerHTML += '<hr>' + e;
+  }
+};
